2fb3ea48c4fcd78fddb3a7771c5f140549764c7d
arturobl00 <arturobl00@msn.com>
2023-09-07 15:26:26 -0600

    Semana 4

--- Estructuras de Datos/Semana 3/code.js
+++ Estructuras de Datos/Semana 3/code.js
@@ -1,3 +1,4 @@
+//Referencia DOM https://developer.mozilla.org/es/docs/Glossary/DOM
 //Trabajando Variables
 //Var de tipo entero
 var entero = 10;
@@ -8,6 +9,10 @@ var cadena = "Mi Saludo";
 //Var de tipo Boleano
 var banderaT = true;
 var banderaF = false;
+
+//Codigo de Ejemplo
+var elementoEjemplo = document.getElementById("ejemplo");
+elementoEjemplo.textContent = "Texto de Ejemplo Practica Unidad 1";
 
 var elemento1 = document.getElementById("miEntero");
 elemento1.textContent = entero;
@@ -26,7 +31,7 @@ elemento5.textContent = banderaF;
 
 //Trabajando con arreglos
 //Arreglo con datos constantes
-var arregloTipo1 = [20,90,10,7,8,12];
+var arregloTipo1 = [20,90,10,7,8,12,20,33,44,56,12,34,78,90,22];
 //Arreglo con datos variados
 var arregloTipo2 = ["uno",1,"dos",2,"tres",3];
 
@@ -54,3 +59,28 @@ function mostrarLista2(){
 }
 
 mostrarLista2();
+
+//Objetos en js
+
+/*
+Notas: 
+*/
+var persona = {
+    nombre: "Arturo",
+    apellidos: "Bustamante Lazcano",
+    edad: 42,
+    vive: true
+}
+
+console.log(persona.nombre);
+
+var elementoPersona = document.getElementById("objetoPersona");
+elementoPersona.textContent = persona.nombre + " " + persona.apellidos + " de " + persona.edad + " años y vive"; 
+
+
+var baseDatos = {
+    id: 1,
+    nombre: "José",
+    edad: 19,
+    carrera: "Sistemas"
+}
