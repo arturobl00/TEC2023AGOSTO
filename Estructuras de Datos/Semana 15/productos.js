0b2a7f5e6b61e54c47964627a53f2c49934bc955
arturobl00 <arturobl00@msn.com>
2023-12-01 08:11:55 -0600

    01/12

---
+++ Estructuras de Datos/Semana 15/productos.js
@@ -0,0 +1,47 @@
+const productos = [
+    {
+        titulo:'Box Engasse',
+        img: 'img/boxengasse.png',
+        precio:'$15.000'
+    },
+    {
+        titulo:'English Horse',
+        img: 'img/englishrose.png',
+        precio:'$25.000'
+    },
+    {
+        titulo:'Knock Nap',
+        img: 'img/knocknap.png',
+        precio:'$35.000'
+    },
+    {
+        titulo:'La Night',
+        img:'img/lanight.png',
+        precio:'$18.000'
+    },
+    {
+        titulo:'Silver All',
+        img: 'img/silverall.png',
+        precio: '$32.000'
+    },
+    {
+        titulo:'Skin Glam',
+        img: 'img/skinglam.png',
+        precio:'$18.000'
+    },
+    {
+        titulo:'Midimix',
+        img: 'img/midimix.png',
+        precio:'$54.000'
+    },
+    {
+        titulo:'Sir Blue',
+        img: 'img/sirblue.png',
+        precio:'$32.000'
+    },
+    {
+        titulo:'Middlesteel',
+        img: 'img/middlesteel.png',
+        precio: '$42.800'
+    }
+]
